3c7ac939b171637fe80bade337df4d32d6413d29
paulwong <paulwong@163.com>
2025-11-22 08:47:44 +0800

    update

--- 01_n8n_server/n8n-upgrade.sh
+++ 01_n8n_server/n8n-upgrade.sh
@@ -3,8 +3,8 @@ cd $BIN_PATH
 
 docker compose down n8n
 # docker compose pull n8n
-docker pull n8nio/n8n:1.107.4
-docker compose up n8n -d
+# docker pull n8n
+# docker compose up n8n -d
 docker compose up n8n -d
 docker compose logs -f
 # docker compose logs n8n
--- 01_n8n_server/open-webui-login.sh
+++ 01_n8n_server/open-webui-login.sh
@@ -2,4 +2,5 @@ BIN_PATH=$(cd `dirname $0`; pwd)
 cd $BIN_PATH
 
 docker compose exec -it open-webui /bin/bash
+#docker compose exec -it open-webui alpine
 # docker compose logs n8n
